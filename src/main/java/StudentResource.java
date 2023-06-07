e1521a460e60b969406fae0559c1ff93aa8e307b
Gustavo <gugaprado10@gmail.com>
2023-06-07 11:00:14 +0200

    asldkjasdlka
    FUCK

--- src/main/java/StudentResource.java
+++ src/main/java/StudentResource.java
@@ -12,39 +12,44 @@ import java.sql.*;
 @Path("/student")
 public class StudentResource {
 
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test() {
+        return "Hello! This is a test";
+    }
+
     String host = "bronto.ewi.utwente.nl";
     String dbName = "dab_di22232b_81";
     String url = "jdbc:postgresql://" + host + ":5432/" +
-            dbName + "?currentSchema=TopicusDatabase";
+            dbName + "?currentSchema=topicusdatabase";
     String username = "dab_di22232b_81";
     String password = "uZQ2Mqk82/Kx6s5l";
-    Connection db = null;
+    Connection db;
 
     public void establishConnection() {
         try {
             db = DriverManager.getConnection(url, username,
                     password);
-        } catch (
-                SQLException e) {
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Path("{id}")
     @GET
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Produces(MediaType.APPLICATION_JSON)
     public Student findStudent(@PathParam("id") int id) throws Exception {
         establishConnection();
         String query = "SELECT * FROM student WHERE student_id = ?";
         PreparedStatement st = db.prepareStatement(query);
-        st.setString(1, String.valueOf(id));
+        st.setInt(1, id);
         ResultSet resultSet = st.executeQuery();
 
-        int queriedId = resultSet.getInt(0);
-        String queriedBsn = resultSet.getString(1);
-        String queriedName = resultSet.getString(2);
-        String queriedBirthDate = resultSet.getString(3);
-        int queriedGuardian_id = resultSet.getInt(4);
+        int queriedId = resultSet.getInt(1);
+        String queriedBsn = resultSet.getString(2);
+        String queriedName = resultSet.getString(3);
+        String queriedBirthDate = resultSet.getString(4);
+        int queriedGuardian_id = resultSet.getInt(5);
 
         Student queriedStudent = new Student(queriedId, queriedBsn, queriedName, queriedBirthDate, queriedGuardian_id);
         return queriedStudent;
