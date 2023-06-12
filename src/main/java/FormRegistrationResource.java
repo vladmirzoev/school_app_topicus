2f857def51fbf0f786e01ed9108d15b8e8e91cc6
Gustavo <gugaprado10@gmail.com>
2023-06-12 12:06:36 +0200

    PUT METHOD WORKS

--- src/main/java/FormRegistrationResource.java
+++ src/main/java/FormRegistrationResource.java
@@ -52,16 +52,41 @@ public class FormRegistrationResource {
                                    @FormParam("guardianName") String guardianName,
                                    @FormParam("telephone1") String telephone1,
                                    @FormParam("telephone2") String telephone2,
-                                   @FormParam("guardianEmail") String email,
-                                   @FormParam("address") String address,
+                                   @FormParam("email") String email,
                                    @FormParam("bsn") String bsn,
                                    @FormParam("birth_date") Date birth_date,
                                    @FormParam("grade") int grade,
-                                   @FormParam("schoolName") String schoolName) throws Exception {
+                                   @FormParam("schoolName") String schoolName,
+                                   @FormParam("address") String address) throws Exception {
         establishConnection();
         int student_id = newStudentID();
         int registration_id = newRegistrationID();
         int school_id = getSchoolID(schoolName);
+
+        if (!accountExists(email)) { //if an account doesnt exist, make a new account entry
+            String account = "INSERT INTO account (account_id, name, address, phone_number_1, phone_number_2) VALUES (?, ?, ?, ?, ?)";
+            PreparedStatement acc = db.prepareStatement(account);
+            acc.setString(1, email);
+            acc.setString(2, guardianName);
+            acc.setString(3, address);
+            acc.setString(4, telephone1);
+            acc.setString(5, telephone2);
+            acc.execute();
+        } else { //if an account exists, update depending on the phone numbers given
+            String account = "UPDATE account SET phone_number_1 = ?, phone_number_2 = ? WHERE account_id = ?";
+            PreparedStatement acc = db.prepareStatement(account);
+            acc.setString(1, telephone1);
+            acc.setString(2, telephone2);
+            acc.setString(3, email);
+            acc.execute();
+        }
+
+        if (!checkGuardianExists(email)) {
+            String guardian = "INSERT INTO guardian VALUES (?)";
+            PreparedStatement guard = db.prepareStatement(guardian);
+            guard.setString(1, email);
+            guard.execute();
+        }
 
         String student = "INSERT INTO student (student_id, bsn, name, birth_date, guardian_id) VALUES (?, ?, ?, ?, ?)";
         PreparedStatement st = db.prepareStatement(student);
@@ -72,34 +97,24 @@ public class FormRegistrationResource {
         st.setString(5, email);
         st.execute();
 
-        String registration = "INSERT INTO registration (registration_id, grade, registration_date, student_id, school_id) VALUES (?, ?, ?, ?, ?)";
+        String registration = "INSERT INTO registration (registration_id, grade, registration_date, student_id, school_id, status) VALUES (?, ?, ?, ?, ?, ?)";
         PreparedStatement reg = db.prepareStatement(registration);
         reg.setInt(1, registration_id);
         reg.setInt(2, grade);
         reg.setDate(3, Date.valueOf(java.time.LocalDate.now()));
         reg.setInt(4, student_id);
         reg.setInt(5, school_id);
+        reg.setString(6, "Under review");
         reg.execute();
 
-        if (!accountExists(email)) {
-            String account = "INSERT INTO account (account_id, name, address, phone_number_1, phone_number_2) VALUES (?, ?, ?, ?, ?)";
-            PreparedStatement acc = db.prepareStatement(account);
-            acc.setString(1, email);
-            acc.setString(2, guardianName);
-            acc.setString(3, address);
-            acc.setString(4, telephone1);
-            acc.setString(5, telephone2);
-            acc.execute();
-        } else {
-            String account = "UPDATE account SET address = ?, phone_number_1 = ?, phone_number_2 = ? WHERE account_id = ?";
-            PreparedStatement acc = db.prepareStatement(account);
-            acc.setString(1, address);
-            acc.setString(2, telephone1);
-            acc.setString(3, telephone2);
-            acc.setString(4, email);
-            acc.execute();
-        }
+    }
 
+    private boolean checkGuardianExists(String email) throws SQLException {
+        String query = "SELECT * FROM guardian WHERE guardian_id = ?";
+        PreparedStatement st = db.prepareStatement(query);
+        st.setString(1, email);
+        ResultSet rs = st.executeQuery();
+        return rs.next();
     }
 
     public void establishConnection() {
